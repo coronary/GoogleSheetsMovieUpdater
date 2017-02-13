3afc16ffc672d35065c7d8c1b7cf2d9af3bcb0d3
corkboard <cjdekalb@gmail.com>
2017-02-12 20:34:09 -0600

    Makes script more applicable to other spreadsheets

--- movie.py
+++ movie.py
@@ -5,53 +5,66 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = "https://spreadsheets.google.com/feeds"
 credentials = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
 gs = gspread.authorize(credentials)
-
-def readSheet(sheet, person, gColumn, rColumn, start, end, Tcolumn):
-	#opens movie spreadsheet
-	gsheet = gs.open(sheet).worksheet(person)
-	#Starts on right number, ends one less than stated
-	for x in range(start, end):
-		gsheet.update_cell(x, Tcolumn, 'X')
-		#Grabs movie title
-		movie = gsheet.cell(x, 1).value
-		try:
-			#Searches for the movie on OMDB
-			baseInfo = Imdb().search_for_title(movie)
-			search = Imdb().get_title_by_id(baseInfo[0]['imdb_id'])
-			#Updates cells with genre and runtime
-			comma = ', '
-			gsheet.update_cell(x, gColumn, comma.join(search.genres))
-			gsheet.update_cell(x, rColumn, str(int(search.runtime/60)) + ' min')
-		except Exception as c:
-			print (str(c) + " at row " + str(x))
-			#if it fucks up it owns up to it
-			gsheet.update_cell(x, gColumn, 'fucked up')
-		gsheet.update_cell(x, Tcolumn, '')
+comma = ', '
 
 def directory():
-	sheets = ['experiment', 'Movie Tracker']
-	cList = ['Collin', 2, 3, 6]
-	mList = ['Molly', 4, 5, 6]
-	person = input('c or m? ').lower()
+	sheetName = 'experiment'
+	gsheet = gs.open(sheetName)
+	#cList = ['Collin', 2, 3, 6]
+	#mList = ['Molly', 4, 5, 6]
+	tempSheets = gsheet.worksheets()
+	sheets = []
+	for x in tempSheets:
+		sheets.append(x.title)
+	while True:
+		try:
+			print (comma.join(sheets))
+			sheet = int(input('Choose the index of the sheet you want '))
+			if sheet >= len(sheets):
+				raise IndexError
+			break
+		except Exception as c:
+			print (c)
+			print ('Must be a valid index between 0 and ' + str(len(sheets)))
 	choice = int(input('movie update(1), days watching movies(2): '))
 	if choice == 2:
-		if person == 'c':
-			time(cList[0], sheets[1], cList[2], cList[3])
-		if person == 'm':
-			time(mList[0], sheets[1], mList[2], mList[3])
+		time(gsheet, sheets[sheet])
 	while True:
 		try:
 			start = int(input('Starting row: '))
 			end = int(input('Ending row: '))
 			break
 		except:
-			print ('Try again')
-	if person == 'c':
-		readSheet(sheets[1],cList[0],cList[1],cList[2],start,end+1,cList[3])
-	elif person == 'm':
-		readSheet(sheets[1],mList[0],mList[1],mList[2],start,end+1,mList[3])
+			print ('Must both be integers')
+	readSheet(gsheet, sheets[sheet], start, end)
 
-def time(person, sheet, Rcolumn, Tcolumn):
+def readSheet(gsheet, sheet, start, end):
+	#opens movie spreadsheet
+	currentSheet = gsheet.worksheet(sheet)
+	columnTitles = currentSheet.row_values(1)
+	movieTitle = columnTitles.index('Title') + 1
+	Tcolumn = columnTitles.index('') + 1
+	gColumn = columnTitles.index('Genre') + 1
+	rColumn = columnTitles.index('Runtime') + 1
+	#Starts on right number, ends one less than stated
+	for x in range(start, end+1):
+		currentSheet.update_cell(x, Tcolumn, 'X')
+		#Grabs movie title
+		movie = currentSheet.cell(x, movieTitle).value
+		try:
+			#Searches for the movie on OMDB
+			baseInfo = Imdb().search_for_title(movie)
+			search = Imdb().get_title_by_id(baseInfo[0]['imdb_id'])
+			#Updates cells with genre and runtime
+			currentSheet.update_cell(x, gColumn, comma.join(search.genres))
+			currentSheet.update_cell(x, rColumn, str(int(search.runtime/60)) + ' min')
+		except Exception as c:
+			print (str(c) + " at row " + str(x))
+			#if it fucks up it owns up to it
+			currentSheet.update_cell(x, gColumn, 'fucked up')
+		currentSheet.update_cell(x, Tcolumn, '')
+
+def time(gsheet, sheet):
 	gsheet = gs.open(sheet).worksheet(person)
 	sum = 0
 	count = 0
